b5b1a538629be70de341be040a270e12e201785e
noisyboy25 <dj402and@gmail.com>
2020-10-13 08:01:24 +0300

    Fix imports

--- src/main/java/com/example/repository/ProductRepository.java
+++ src/main/java/com/example/repository/ProductRepository.java
@@ -2,7 +2,6 @@ package com.example.repository;
 
 import com.example.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
