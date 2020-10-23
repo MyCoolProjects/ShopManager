6924377e0c126db7f03daba961da83a35e840fd9
noisyboy25 <dj402and@gmail.com>
2020-10-23 14:30:36 +0300

    Add DefaultPageController for react-router, fix application.properties

---
+++ src/main/java/com/example/controller/DefaultPageController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultPageController {
+    @GetMapping(path = {"/home/**","/product/**","/catalog/**"})
+    public String defaultPage() {
+        return "/app.html";
+    }
+    @GetMapping(path = "/")
+    public String homePage() {
+        return "redirect:/home/";
+    }
+
+}
--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("${api-path:/api}")
 public class ProductController {
 
     @Autowired
